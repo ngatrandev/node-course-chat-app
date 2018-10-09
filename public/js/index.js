d6b6e1eaf6bdbd4ddd91d772c58e579f0901dc91
nga <ngatrandev@gmail.com>
2018-10-09 12:57:19 +0700

    Emit newMessage on createMessage to every single connection

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,10 @@ var socket = io();
 socket.on('connect', function () {
     //dùng regular function để tránh lỗi trên các browser và device
     console.log('Connected to server');
-    socket.emit('createMessage', {
-        form: 'ngatrandev',
-        text: 'Hey, that works for me'
-    })//phát ra event đến server
+    // socket.emit('createMessage', {
+    //     form: 'ngatrandev',
+    //     text: 'Hey, that works for me'
+    // })//phát ra event đến server
 });
 socket.on('disconnect', function () {
     console.log('Disconnected from the server');
--- server/server.js
+++ server/server.js
@@ -15,13 +15,20 @@ io.on('connection', (socket)=> {//tạo event với name mặc định
     });
     //terminal (bên server) viết trong file server.js
     // browser (bên client) viết trong file index.html
-    socket.emit('newMessage', {
-        from: 'GAU',
-        text: "See you then",
-        createAt: 123123
-    }) // emit event: phát ra event để phía client listen.
+    // socket.emit('newMessage', {
+    //     from: 'GAU',
+    //     text: "See you then",
+    //     createAt: 123123
+    // }) // emit event: phát ra event để phía client listen.
     socket.on('createMessage', (message)=> {
         console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
+    //socket.emit() emit an event to a single connection/ phát event đến 1 client
+    //io.emit() emit an event to every single connection/ phát event đến tất cả client
     })// listen event từ phía client
 });
 
