f8a73c95a72afae64d0983b37c7f2d5184843066
nga <ngatrandev@gmail.com>
2018-10-11 19:34:17 +0700

    Add mustache.js for message template

--- public/js/index.js
+++ public/js/index.js
@@ -13,20 +13,36 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
     var formatedTime = moment(message.createdAt).format('h:mm a');
-    var li = jQuery('<li></li>');
-    li.text(`${message.from}: ${formatedTime} ${message.text} `);// add text vào tab <li></li>
-    jQuery('#messages').append(li);//gắn các tab li(list) vào tab ol(order list)
+    var template = jQuery('#message-template').html();
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formatedTime
+    });
+    jQuery('#messages').append(html);
+    // var formatedTime = moment(message.createdAt).format('h:mm a');
+    // var li = jQuery('<li></li>');
+    // li.text(`${message.from}: ${formatedTime} ${message.text} `);// add text vào tab <li></li>
+    // jQuery('#messages').append(li);//gắn các tab li(list) vào tab ol(order list)
     //jQuery lấy theo id dùng dấu #
 });//tạo event listener
 
 socket.on('newLocationMessage', function (message) {
     var formatedTime = moment(message.createdAt).format('h:mm a');
-    var li = jQuery('<li></li>');
-    var a = jQuery('<a target="_blank">My current location</a>');
-    li.text(`${message.from} ${formatedTime} `);
-    a.attr('href', message.url);
-    li.append(a);
-    jQuery('#messages').append(li);
+    var template = jQuery('#location-message-template').html();
+    var html = Mustache.render(template, {
+        url: message.url,
+        from: message.from,
+        createdAt: formatedTime
+    });
+    jQuery('#messages').append(html);
+
+    // var li = jQuery('<li></li>');
+    // var a = jQuery('<a target="_blank">My current location</a>');
+    // li.text(`${message.from} ${formatedTime} `);
+    // a.attr('href', message.url);
+    // li.append(a);
+    // jQuery('#messages').append(li);
 });
 
 socket.emit('createMessage', {
