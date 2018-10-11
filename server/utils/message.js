5e5f82753472a82862dc4c79b1fe81d63e130cd9
nga <ngatrandev@gmail.com>
2018-10-11 16:36:29 +0700

    Format timestamps using momentjs

--- server/utils/message.js
+++ server/utils/message.js
@@ -1,15 +1,16 @@
+const moment = require('moment');// library time stamp
 const generateMessage = (from, text) => {
     return {
         from,
         text,
-        createdAt: new Date().getTime()
+        createdAt: moment().valueOf()
     }
 };
 const generateLocationMessage = (from, latitude, longitude)=> {
     return {
         from,
         url: `https://www.google.com/maps?q=${latitude},${longitude}`,
-        createdAt: new Date().getTime()
+        createdAt: moment().valueOf()
     }
 }
 module.exports = {
